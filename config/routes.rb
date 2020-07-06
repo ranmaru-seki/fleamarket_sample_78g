0b71160be960e572ec648e763e710695187682fb
kouji-s222 <kouji620222@gmail.com>
2020-07-06 21:39:38 +0900

    商品詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :new] 
+  resources :products, only: [:index, :new, :show] 
   resources :users, only: [:new, :edit, :update, :show, :create]
 end
