cd5ee1c7c7a480b8afaa60192cceee01605c66ee
kouji-s222 <kouji620222@gmail.com>
2020-07-06 14:30:21 +0900

    スプリントレビュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :new] 
   resources :users, only: [:new, :edit, :update, :show, :create]
