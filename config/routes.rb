750b00fe6c6f487ad0715502c6874e8003092c4b
kouji-s222 <kouji620222@gmail.com>
2020-07-09 08:03:02 +0900

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :new, :create, :show] do
+    resources :orders, only: [:new] do
+      collection do
+        get :create
+      end # 一時的にcreateをcollectionでビュー確認しています。
+    end
+  end
+  resources :users, only: [:new, :edit, :update, :show, :create] do
+    collection do 
+      get :logout
+    end
+  end
+  resources :creditcards, only: [:show, :new]
+end
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -7,8 +24,6 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
 
-  root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :new, :create, :show]
-  resources :users, only: [:new, :edit, :update, :show, :create, :destroy]
 end
