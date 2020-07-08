77b694ec55f22f88c8187c782d9bdcff0a5e6945
kouji-s222 <kouji620222@gmail.com>
2020-07-09 08:07:49 +0900

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -15,15 +15,3 @@ Rails.application.routes.draw do
   end
   resources :creditcards, only: [:show, :new]
 end
-
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-  devise_scope :user do
-    get 'addresses', to: 'users/registrations#new_address'
-    post 'addresses', to: 'users/registrations#create_address'
-  end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :new, :create, :show]
-end
