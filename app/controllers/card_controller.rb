5701e0c898f5444277a66f331cdb8ae2765460b5
kouji-s222 <kouji620222@gmail.com>
2020-07-15 03:54:49 +0900

    カード登録機能

---
+++ app/controllers/card_controller.rb
@@ -0,0 +1,80 @@
+class CardController < ApplicationController
+  require "payjp"
+
+  def new
+    @card = Card.where(user_id: current_user.id)
+    redirect_to card_path(current_user.id) if @card.exists?
+  end
+
+  def create
+    Payjp.api_key = Rails.application.credentials[:payjp][:access_key]
+
+    if params["payjp_token"].blank?
+      redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
+    else
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params["payjp_token"],
+        metadata: {user_id: current_user.id}
+      )
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+      else
+        redirect_to action: "create"
+      end
+    end
+  end
+
+  def show
+    # ログイン中のユーザーのクレジットカード登録の有無を判断
+    @card = Card.find_by(user_id: current_user.id)
+    if @card.blank?  # 未登録なら新規登録画面に
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = Rails.application.credentials[:payjp][:access_key]
+      # ログインユーザーのクレジットカード情報からPay.jpに登録されているカスタマー情報を引き出す
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @customer_card = customer.cards.retrieve(@card.card_id)
+
+      ##カードのアイコン表示のための定義づけ
+      @card_brand = @customer_card.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.png"
+      when "JCB"
+        @card_src = "jcb.png"
+      when "MasterCard"
+        @card_src = "master.png"
+      when "American Express"
+        @card_src = "amex.png"
+      when "Diners Club"
+        @card_src = "diners.png"
+      when "Discover"
+        @card_src = "discover.png"
+      end
+
+      @exp_month = @customer_card.exp_month.to_s
+      @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+    end
+  end
+
+  def destroy
+    @card = Card.find_by(user_id: current_user.id)
+    if @card.blank?
+      # 未登録なら新規登録画面に
+      redirect_to action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials[:payjp][:access_key]
+      # ログインユーザーのクレジットカード情報からPay.jpに登録されているカスタマー情報を引き出す
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      customer.delete
+      @card.delete
+      # 削除が完了しているか判断
+      if @card.destroy
+      else
+        redirect_to card_path(current_user.id), alert: "削除できませんでした。"
+      end
+    end
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates :name, :email, :family_name, :first_name, :family_name_kana, :first_name_kana, presence: true
   validates :name, :email, uniqueness: true
   has_one :address
+  has_one :card, dependent: :destroy
   has_many :products
 
   extend ActiveHash::Associations::ActiveRecordExtensions
