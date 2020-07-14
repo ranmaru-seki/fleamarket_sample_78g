81dda6cdbf837e57f36b16dbde019aab2af389e0
ranmaru-seki <ranma19.sar@gmail.com>
2020-07-14 09:35:10 +0900

    small-changes

--- app/assets/javascripts/category_tree.js
+++ app/assets/javascripts/category_tree.js
@@ -12,43 +12,39 @@ $(function() {
     $(".category-overlay").css('display', 'block');
   });
 
-
-  $(document).on("mouseover", ".show_hover", function () {
-    $(".displayNone").css('display', 'block');
-    $(".category-overlay").css('display', 'block');
-  });
-
-  //カテゴリ外をクリックすると消える
+  //Overlayを使い、カテゴリ枠外をクリックすると消える
   $(".category-overlay").on('click', function() {
     $('.category-overlay').css('display', 'none');
     $('.show_hover').css('display', 'none');
   });
 
+  //親カテゴリにマウスが乗ったらイベント発火
   $(".parent_category").on("mouseover", function() {
-    var id = this.id//どのリンクにマウスが乗ってるのか取得します
-    $(".child_category").remove();//一旦出ている子カテゴリ消します！
-    $(".grand_child_category").remove();//孫、てめえもだ！
+    var id = this.id　//どのリンクにマウスが乗ってるのか取得します
+    $(".child_category").remove();　
+    $(".grand_child_category").remove();　
     $.ajax({
       type: 'GET',
       url: '/products/get_toppage_children',
-      data: {parent_id: id},//どの親の要素かを送ります　params[:parent_id]で送られる
+      data: {parent_id: id},　
       dataType: 'json'
     }).done(function(children) {
-      children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
-        var html = buildChildHTML(child);//HTMLにして
-        $(".children_list").append(html);//リストに追加します
+      children.forEach(function (child) {
+        var html = buildChildHTML(child);
+        $(".children_list").append(html);
       })
     });
   });
 
-  // 孫カテゴリを追加する処理です　基本的に子要素と同じです！
+  // 孫カテゴリを追加する処理、基本的に子要素と同じ
   function buildGrandChildHTML(child){
     var html =`<a class="grand_child_category show_hover" id="${child.id}"
                href="/category/${child.id}">${child.name}</a>`;
     return html;
   }
 
-  $(document).on("mouseover", ".child_category", function () {//子カテゴリーのリストは動的に追加されたHTMLのため
+  //子カテゴリーのリストは動的に追加されたHTMLのため
+  $(document).on("mouseover", ".child_category", function () {
     var id = this.id
     $.ajax({
       type: 'GET',
