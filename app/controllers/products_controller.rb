d162a59734899efc7e8454df8fd74dd2fec92f77
katlao0829 <nskj.m1958@gmail.com>
2020-07-17 18:43:36 +0900

    Product-Exibit-alert

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,24 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :show_sold, :edit, :update, :destroy]
+  before_action :category_array_for_new, only: [:new, :create]
+  before_action :category_array_for_edit, only: [:edit, :update]
+  
 
   def index
-    @parents = Category.where(ancestry: nil);
+    @parents = Category.where(ancestry: nil)
     @product_array = Product.all.reverse
     @lady_product_array = @product_array.select { |product| product.category.root.id == 1 }
   end
 
   def show
-    @parents = Category.where(ancestry: nil);
-    @product = Product.find(params[:id])
+    @parents = Category.where(ancestry: nil)
     @image = Image.find_by(product_id: @product.id)
     @image_array = Image.where(product_id: @product.id)
     @product_array = Product.where(category_id: @product.category_id).reverse
   end
 
   def show_sold
-    @parents = Category.where(ancestry: nil);
-    @product = Product.find(params[:id])
+    @parents = Category.where(ancestry: nil)
     @image = Image.find_by(product_id: @product.id)
     @image_array = Image.where(product_id: @product.id)
     @product_array = Product.where(category_id: @product.category_id).reverse
@@ -25,53 +27,36 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.images.new
-    @category_parent_array = ["---"]
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-    end
   end
 
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = '商品出品が完了しました'
       redirect_to product_path(@product.id)
     else
-      render :new
+      flash[:alert] = '必須項目を入力してください'
+      redirect_to action: "new"
     end
   end
 
   def edit
-    @product = Product.find(params[:id])
     Image.where(product_id: @product.id)
-    grandchild_category = @product.category
-    child_category = grandchild_category.parent
-
-    @category_parent_array = ["#{@product.category.root.name}"]
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-    end
-    @category_child_array = [name: "---", id: ""]
-    Category.where(ancestry: child_category.ancestry).each do |children|
-      @category_child_array << children
-    end
-    @category_grandchild_array = [name: "---", id: ""]
-    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
-      @category_grandchild_array << grandchildren
-    end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
+      flash[:notice] = '商品情報を更新しました'
       redirect_to product_path(@product.id)
     else
+      flash.now[:alert] = '必須項目を入力してください'
       render :edit
     end
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
+    flash[:alert] = '商品を削除しました'
     redirect_to root_path
   end
 
@@ -119,5 +104,32 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name, :item_info, :brand, :status_id, :size, :delivery_id, :region_id, :day_id, :price, :category_id, images_attributes: [:src, :_destroy, :id]).merge(user_id: current_user.id)
   end
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def category_array_for_new
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+  end
+
+  def category_array_for_edit
+    grandchild_category = @product.category
+    child_category = grandchild_category.parent
+    @category_parent_array = ["#{@product.category.root.name}"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+    @category_child_array = [name: "---", id: ""]
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_child_array << children
+    end
+    @category_grandchild_array = [name: "---", id: ""]
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchild_array << grandchildren
+    end
+  end
 
 end
