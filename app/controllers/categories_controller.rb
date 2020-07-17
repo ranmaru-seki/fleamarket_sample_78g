726eff605548b10aa685a42127d185dc10202b4c
ranmaru-seki <ranma19.sar@gmail.com>
2020-07-17 13:29:19 +0900

    create-category-page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,15 @@
 class CategoriesController < ApplicationController
 
   def show
+    products = Product.all.reverse
     @parents = Category.where(ancestry: nil);
     @category = Category.find(params[:id])
+    if @category.ancestry == nil
+      @category_product_array = products.select { |product| product.category.root.id == @category.id }
+    elsif @category.ancestry.include?("/")
+      @category_product_array = products.select { |product| product.category_id == @category.id }
+    else
+      @category_product_array = products.select { |product| product.category.parent.id == @category.id }
+    end
   end
-
 end
